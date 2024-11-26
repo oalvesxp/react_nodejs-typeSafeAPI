7c569c574d546b3338044d70cc68fbc24e6adc05
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-11-25 23:05:39 -0300

    fe: test listUsers

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react'
+import { listUsers, type ListUsers200Item } from './http/generated/api'
+
 function App() {
+  const [users, setUsers] = useState<ListUsers200Item[]>([])
+
+  useEffect(() => {
+    listUsers().then(users => {
+      setUsers(users.data)
+    })
+  })
+
   return (
     <>
-      <h1>Hello world!</h1>
+      <ul>
+        {users.map(user => {
+          return (
+            <li key={user.id}>{user.name}</li>
+          )
+        })}
+      </ul>
     </>
   )
 }
